7fc0acd529ec6c0dac06b5eed41109ff2dd4e4cd
Noah <shgustmd6865@naver.com>
2022-04-26 00:08:40 +0900

    Feat: "S3 기본 설정 추가"

--- src/main/java/com/team4/sns/serviceImpl/TestServiceImpl.java
+++ src/main/java/com/team4/sns/serviceImpl/TestServiceImpl.java
@@ -2,17 +2,28 @@ package com.team4.sns.serviceImpl;
 
 import com.team4.sns.mapper.TestMapper;
 import com.team4.sns.service.TestService;
+import com.team4.sns.util.S3Util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
 
     private final TestMapper testMapper;
+    private final S3Util s3Util;
 
     @Override
     public String test() {
         return testMapper.test();
+    }
+
+    @Override
+    public void uploadImageTest(List<MultipartFile> imageFileList) throws IOException {
+        s3Util.uploadObject(imageFileList);
     }
 }
